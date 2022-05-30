6ab5f4ec1dc80450f525c37d965b5c70ac894267
frogindreams <frogharvard@gmail.com>
2022-05-30 17:39:07 +0700

    the last test was passed

--- include/lmodules.h
+++ include/lmodules.h
@@ -5,7 +5,7 @@ using std::vector;
 #ifndef LMODULES_H
 #define LMODULES_H
 
-vector<double> get_gradation(int, int, vector<vector<double>>, vector<int>);
+vector<double> get_gradation(int, int, vector<vector<double>>, vector<double>);
 vector<int> get_free_vars();
 
 #endif
